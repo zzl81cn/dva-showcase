7c8986ae939fc44cc1ce590683ae455d9709786d
zzl81cn <zzl81cn@163.com>
2018-05-14 14:10:04 +0800

    Modify first showcase

--- src/routes/Users.js
+++ src/routes/Users.js
@@ -2,4 +2,14 @@ import React from 'react';
 import { connect } from 'dva';
 import styles from './Users.css';
 import UsersComponent from '../components/Users/Users';
-import MainLayout from ''
+import MainLayout from '../components/MainLayout/MainLayout';
+
+function Users({ location }) {
+  return (
+    <MainLayout location={location}>
+      <div className={styles.normal}><UserComponent/></div>
+    </MainLayout>
+  )
+}
+
+export default connect()(Users);
